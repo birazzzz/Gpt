4ae842e738fd4aa051102775b20540383cfd1de1
birazzzz <59414597+birazzzz@users.noreply.github.com>
2023-02-13 19:30:22 +0545

    big components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-import { Brand, Cta, Navbar} from './components';
-import { Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
-import './App.css'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Cta, Brand, Navbar } from './components';
 
-const App = () => {
-  return (
-    <div classname="App">
-      <div classname="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <Cta />
-      <Blog />
-      <Footer />
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <Cta />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './navbar.css';
+import {RiMenuline, RiCloseline } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+
+//BEM  > Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className="Gpt3__navbar">
+      <div className="Gpt3__navbar-links">
+        <div className="gpt3">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
